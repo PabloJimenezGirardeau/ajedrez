bc0bb4e058654845176ec60208b74035a5204f73
PabloJimenezGirardeau <146074676+PabloJimenezGirardeau@users.noreply.github.com>
2023-12-08 16:12:26 +0100

    README.d

--- README.d
+++ README.d
@@ -1,5 +1,5 @@
 # Ajedrez #
-## Este programa incluye un juego de ajedrez que registra los movimientos del 
+Este programa incluye un juego de ajedrez que registra los movimientos del 
 jugador en un archivo. Posteriormente la consola solicita al jugadorintroducir 
 uno de los movimientos realizados anteriormente y se muestra el tablero y el  
-orden de las fichas corresponidentes ##
+orden de las fichas corresponidentes 
